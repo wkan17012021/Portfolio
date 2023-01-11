da63ee8ddfb8f36c62870b6d3b09a7853a303a43
wkan17012021 <wkan@hotmail.co.uk>
2023-01-11 23:48:43 +0000

    draft hero jumbo section

--- portfolio_react_spa/src/App.jsx
+++ portfolio_react_spa/src/App.jsx
@@ -1,22 +1,18 @@
 import ResponsiveAppBar from "./sections/navigation/Nav"
 import ControlledAccordions from "./sections/workexp/Accordion"
+import Hero from "./sections/hero/MainHero"
 import './assets/reset.css'
 import './assets/fonts.css'
+import './assets/global.css'
+import "./css/hero.css"
 
 function App() {
   
 
   return (
     <div className="App">
-      <div>
-      <link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-  integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-  crossorigin="anonymous"
-/>
-      </div>
      <ResponsiveAppBar />
+     <Hero />
      <ControlledAccordions />
     </div>
   )
---
+++ portfolio_react_spa/src/sections/hero/MainHero.jsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+
+const Hero = () => {
+  return (
+    <header class="hero">
+      <div class="wrapper hero__wrapper">
+        <div class="hero__content">
+          <h1 class="hero__headline">William Kan</h1>
+          <div class="flow">
+            <p>
+              Front-End Developer | Netizen | Bug Catcher{" "}
+              
+            </p>
+            <img
+                class="pokemon-caterpie"
+                src="src/assets/caterpie.png"
+                alt="Pokemon creature: Caterpie's face"
+              />
+          </div>
+        </div>
+        <img class="bkgnd-profile" src="src/assets/profile-photo.svg" alt="" />
+      </div>
+    </header>
+  );
+};
+
+export default Hero;
