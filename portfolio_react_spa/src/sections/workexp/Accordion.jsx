e5b9aa1b97bc23f75ff53313452cc51c0ef584be
wkan17012021 <wkan@hotmail.co.uk>
2023-01-12 22:55:01 +0000

    increase caterpie size, reformat work exp data

--- portfolio_react_spa/src/sections/workexp/Accordion.jsx
+++ portfolio_react_spa/src/sections/workexp/Accordion.jsx
@@ -15,7 +15,12 @@ export default function ControlledAccordions() {
  
   return (
     <section className='work-exp-container'>
-      {accordionEntry.jobs.map(job => {
+       <Accordion disabled>
+        <AccordionSummary>
+          <Typography>Tech Experience</Typography>
+        </AccordionSummary>
+      </Accordion>
+      {accordionEntry.techJobs.map(job => {
         return (
           <Accordion expanded={expanded === job.panelNum} onChange={handleChange(`${job.panelNum}`)}>
         <AccordionSummary
@@ -36,61 +41,33 @@ export default function ControlledAccordions() {
       </Accordion>
         )
       })}
-      
-      {/* <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+      <Accordion disabled>
+        <AccordionSummary>
+          <Typography>Non-Tech Experience</Typography>
+        </AccordionSummary>
+      </Accordion>
+      {accordionEntry.nonTechjobs.map(job => {
+        return (
+          <Accordion expanded={expanded === job.panelNum} onChange={handleChange(`${job.panelNum}`)}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2bh-content"
-          id="panel2bh-header"
+          aria-controls={job.ariaControls}
+          id="panel1bh-header"
         >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>Users</Typography>
-          <Typography sx={{ color: 'text.secondary' }}>
-            You are currently not an owner
+          <Typography sx={{ width: '40%', flexShrink: 0 }}>
+           {job.title}
           </Typography>
+          <Typography sx={{ color: 'text.secondary' }}>{job.year}</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus,
-            varius pulvinar diam eros in elit. Pellentesque convallis laoreet
-            laoreet.
+            {job.body}
           </Typography>
         </AccordionDetails>
       </Accordion>
-      <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3bh-content"
-          id="panel3bh-header"
-        >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>
-            Advanced settings
-          </Typography>
-          <Typography sx={{ color: 'text.secondary' }}>
-            Filtering has been entirely disabled for whole web server
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
-            amet egestas eros, vitae egestas augue. Duis vel est augue.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel4bh-content"
-          id="panel4bh-header"
-        >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>Personal data</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
-            amet egestas eros, vitae egestas augue. Duis vel est augue.
-          </Typography>
-        </AccordionDetails>
-      </Accordion> */}
+        )
+      })}
+
     </section>
   );
 }
