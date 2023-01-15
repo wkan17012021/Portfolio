04eb684fbd8c2e9929a5a31fa4d6fec83b819fd2
wkan17012021 <wkan@hotmail.co.uk>
2023-01-15 22:12:58 +0000

    move proj data to separate file update mui styles

---
+++ portfolio_react_spa/src/data/projList.js
@@ -0,0 +1,17 @@
+export const projList = [
+  {
+    id: 1,
+    title: "Road Less Travelled",
+    duration: "2021 - Present",
+    url: "https://theroadlesstravelled.netlify.app/",
+    img: "src/assets/rlt_placeholder.webp",
+    alt: "placeholder image of Road Less Travelled Project: geologically themed travel website",
+  },
+  {
+    id: 2,
+    title: "Pool Club",
+    duration: "2023 - Present",
+    img: "src/assets/pool_placeholder.webp",
+    alt: "placeholder image of swimming pool",
+  },
+];
