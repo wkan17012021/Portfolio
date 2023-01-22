9e29656a429714f398518ca417c9d2b25753a76a
wkan17012021 <wkan@hotmail.co.uk>
2023-01-22 21:51:15 +0000

    cleanup markup convert to mui comps

--- portfolio_react_spa/src/sections/hero/MainHero.jsx
+++ portfolio_react_spa/src/sections/hero/MainHero.jsx
@@ -1,31 +1,53 @@
 import * as React from "react";
 import { Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
 const Hero = () => {
   return (
-    <header className="hero"
-    sx={{
-      position: 'relative',
-      color: 'red'
-    }}>
+    <section className="hero">
       <div className="wrapper hero__wrapper">
+        <Container
+          className="hero__content"
+        >
+          <Typography variant="h1" fontWeight={700}>
+            William Kan
+          </Typography>
+        </Container>
         <div className="hero__content">
-        <Typography variant="h1">William Kan</Typography>
-          <div className="flow">
-            <Typography>
-              Front-End Developer <span className="pipe">|</span> Netizen <span className="pipe">|</span>  Bug Catcher{" "}
+          <Container
+            className="flow"
+            sx={{
+              marginTop: "0.5rem",
+            }}
+          >
+            <Typography variant="body1" fontWeight={600}>
+              Front-End Developer <span className="pipe">|</span> Netizen{" "}
+              <span className="pipe">|</span> Bug Catcher{" "}
             </Typography>
-            <img
-                className="pokemon-caterpie"
-                src="src/assets/caterpie.png"
-                alt="Pokemon creature: Caterpie's face"
-              />
-          </div>
-          <Typography>CV available upon request</Typography>
+            <Box
+              component="img"
+              className="pokemon-caterpie"
+              src="src/assets/caterpie.png"
+              alt="Pokemon creature: Caterpie's face"
+            ></Box>
+          </Container>
+          <Container
+          sx={{
+            marginTop: "0.5rem",
+          }}>
+             <Typography variant="body1">CV available upon request</Typography>
+          </Container>
+         
         </div>
-        <img className="bkgnd-profile" src="src/assets/profile-photo.svg" alt="" />
+        <Box
+          component="img"
+          className="bkgnd-profile"
+          src="src/assets/profile-photo.svg"
+          alt="hiker sitting on a rock looking out into the sunset with a cityscape backdrop"
+        ></Box>
       </div>
-    </header>
+    </section>
   );
 };
 
