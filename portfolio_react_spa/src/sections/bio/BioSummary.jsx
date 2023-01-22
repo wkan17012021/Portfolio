757006bb04bff33f3f30525dd2362eab65af410d
wkan17012021 <wkan@hotmail.co.uk>
2023-01-22 22:18:48 +0000

    make comp two col grid responsive

--- portfolio_react_spa/src/sections/bio/BioSummary.jsx
+++ portfolio_react_spa/src/sections/bio/BioSummary.jsx
@@ -1,29 +1,46 @@
 import React from "react";
 import StorySetCoder from "./StorySetCoder.jsx";
 import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import "../../css/storysetsvg.css";
 
 const Bio = () => {
   return (
-    <section id="bio-section" className="scroll-mt-offset">
-       <Typography variant="h2" mt={3}  gutterBottom>
+    <Container id="bio-section" className="scroll-mt-offset">
+    <Box
+    >
+        <Grid container spacing={3}>
+        <Grid item xs={12} sm={10} md={8} lg={4}>
+          <Container>
+            <Typography variant="h2" mt={4}  gutterBottom>
         Biography
       </Typography>
-      <div>
-        <h2>
+          </Container>
+                <Container>
+        <Typography variant="body1"
+        paddingBottom={4}>
           Entry-level developer seeking web and software development
           opportunities within South West London or fully remote roles
-        </h2>
-        <p>
-          I am keen to continue learning new technologies and programming tools
+        </Typography>
+        <hr />
+      </Container>
+      <Container 
+      sx={{marginTop: 4}}>
+        <Typography variant="body1">
+        I am keen to continue learning new technologies and programming tools
           so that I can enhance my capabilities as a developer and aspire to
           provide support for front- and back-end innovative solutions
-        </p>
-      </div>
-      <div>
-        <StorySetCoder />
-      </div>
-    </section>
+        </Typography>
+      </Container>
+        </Grid>
+        <Grid item xs={12} sm={10} md={8} lg={8}>
+         <StorySetCoder />
+        </Grid>
+      </Grid>
+     </Box>
+    </Container>
   );
 };
 
