11e2fbc93d112980b884a933688c944d2229526f
wkan17012021 <wkan@hotmail.co.uk>
2023-07-27 11:42:24 +0100

    rm storyset img from prof skills comp

--- portfolio_react_spa/src/components/layouts/FluidContainer.jsx
+++ portfolio_react_spa/src/components/layouts/FluidContainer.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import Box from "@mui/material/Box";
-import StorySetTeam from "../../sections/skills/StorySetTeam";
+import SkillsMarquee from "../../sections/marquee/SkillsMarquee";
 
 const FluidContainer = ({ children }) => {
   return (
     <section className="fluid-container" style={{marginTop: '-60px'}}>
       {children}
-      <Box>
-        <StorySetTeam />
-      </Box>
+      <SkillsMarquee />
     </section>
   );
 };
