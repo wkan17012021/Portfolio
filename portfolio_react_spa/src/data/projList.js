0b36d1c9e8f508f6ef24ae89e504d6cfbdebc4f3
wkan17012021 <wkan@hotmail.co.uk>
2023-07-29 21:46:19 +0100

    added oyw project and blogged about it

--- portfolio_react_spa/src/data/projList.js
+++ portfolio_react_spa/src/data/projList.js
@@ -1,19 +1,29 @@
+import OYWProj from "../assets/oyw-london_converted.webp";
 import RLTProj from "../assets/rlt_placeholder.webp";
 import PoolProj from "../assets/pool_placeholder.webp";
 
 export const projList = [
   {
     id: 1,
+    title: "One Young World",
+    duration: "2022 - Present",
+    url: "https://www.oneyoungworld.com/",
+    img: OYWProj,
+    alt: "placeholder image of One Young World Project: stage presenters at the London Summit",
+    brief: "https://the-dog-can-blog.hashnode.dev/a-reflection-on-my-involvement-as-a-digital-apprentice-at-oyw"
+  },
+  {
+    id: 2,
     title: "Road Less Travelled",
-    duration: "2021 - Present",
+    duration: "December 2021",
     url: "https://theroadlesstravelled.netlify.app/",
     img: RLTProj,
     alt: "placeholder image of Road Less Travelled Project: geologically themed travel website",
   },
   {
-    id: 2,
+    id: 3,
     title: "Community Pool Club",
-    duration: "2023 - Present",
+    duration: "April 2023 (requires 'warming up' Pantheon's cold start dev server, please be patient on page load 🙏🏽)",
     url: "https://dev-hampton-pool.pantheonsite.io/",
     img: PoolProj,
     alt: "placeholder image of swimming pool",
