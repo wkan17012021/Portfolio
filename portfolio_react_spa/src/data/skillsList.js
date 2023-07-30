c1032f5ec2380a62d8d6e0c8460cf3972dc029d3
wkan17012021 <wkan@hotmail.co.uk>
2023-07-30 13:04:12 +0100

    resolve imports for filepaths prof skills icons

--- portfolio_react_spa/src/data/skillsList.js
+++ portfolio_react_spa/src/data/skillsList.js
@@ -1,84 +1,93 @@
+import HTMLicon from "../assets/icon-html-code.svg";
+import CSSicon from "../assets/icon-css-code.svg";
+import JSicon from "../assets/icon-javascript.svg";
+import Drupalicon from "../assets/icon-website.svg";
+import Twigicon from "../assets/icon-website-template.svg";
+import RWDicon from "../assets/icon-responsive-design.svg";
+import Collabicon from "../assets/icon-community-collaboration.svg";
+import VersionControlicon from "../assets/icon-family-tree.svg";
+import TechSupporticon from "../assets/icon-headphones.svg";
+import Apprenticeicon from "../assets/icon-online-learning.svg";
+import Presentationicon from "../assets/icon-presentation.svg";
+
 export const skills = [
   {
     id: 1,
     skillTitle: "HTML5",
     source: "Bootcamp, independent study and work-based application",
-    img: "./src/assets/icon-html-code.svg",
-    alt: "cartoon icon of HTML"
+    img: HTMLicon,
+    alt: "cartoon icon of HTML",
   },
   {
     id: 2,
     skillTitle: "CSS3",
     source: "Bootcamp, independent study and work-based application",
-    img: "./src/assets/icon-css-code.svg",
-    alt: "cartoon icon of CSS"
+    img: CSSicon,
+    alt: "cartoon icon of CSS",
   },
   {
     id: 3,
     skillTitle: "JavaScript ES6",
     source: "Bootcamp, independent study and work-based application",
-    img: "./src/assets/icon-javascript.svg",
-    alt: "cartoon icon of Javascript intials"
+    img: JSicon,
+    alt: "cartoon icon of Javascript intials",
   },
   {
     id: 4,
     skillTitle: "Drupal 9",
     source: "Work-based site builder, admin and maintenance role",
-    img: "./src/assets/icon-website.svg",
-    alt: "cartoon icon of website on a computer"
+    img: Drupalicon,
+    alt: "cartoon icon of website on a computer",
   },
   {
     id: 5,
     skillTitle: "Twig templating",
     source: "Work-based frontend Drupal themer - presentation layer",
-    img: "./src/assets/icon-website-template.svg",
-    alt: "cartoon icon of website design templating"
+    img: Twigicon,
+    alt: "cartoon icon of website design templating",
   },
   {
     id: 6,
     skillTitle: "RWD and accessibility",
     source: "Bootcamp, independent study and work-based application",
-    img: "./src/assets/icon-responsive-design.svg",
-    alt: "icon of responsive design on multi-screen devices"
+    img: RWDicon,
+    alt: "icon of responsive design on multi-screen devices",
   },
   {
     id: 7,
     skillTitle: "Junior Dev Team Member",
-    source:
-      "Exp gathering requirements, debugging, code production",
-      img: "./src/assets/icon-community-collaboration.svg",
-      alt: "icon of stick people high-fiving"
+    source: "Exp gathering requirements, debugging, code production",
+    img: Collabicon,
+    alt: "icon of stick people high-fiving",
   },
   {
     id: 8,
     skillTitle: "Version control",
     source: "Exposure to production code base and feature-based workflow",
-    img: "./src/assets/icon-family-tree.svg",
-    alt: "cartoon icon of tree branch diagram"
+    img: VersionControlicon,
+    alt: "cartoon icon of tree branch diagram",
   },
   {
     id: 9,
-    skillTitle:
-      "Tech support for conference",
+    skillTitle: "Tech support for conference",
     source:
       "Dispute resolution, reporting and escalating bugs and communicating fixes",
-      img: "./src/assets/icon-headphones.svg",
-      alt: "cartoon icon of headphones"
+    img: TechSupporticon,
+    alt: "cartoon icon of headphones",
   },
   {
     id: 10,
-    skillTitle:
-      "Apprenticeship program",
-    source: "Learned about web technologies and the software development lifecycle",
-    img: "./src/assets/icon-online-learning.svg",
-    alt: "cartoon icon of scholar cap above a open laptop notebook" 
+    skillTitle: "Apprenticeship program",
+    source:
+      "Learned about web technologies and the software development lifecycle",
+    img: Apprenticeicon,
+    alt: "cartoon icon of scholar cap above a open laptop notebook",
   },
   {
     id: 11,
     skillTitle: "Soft Skills",
-    source:
-      "Deliver presentations, reporting and support junior staff",
-      img: "./src/assets/icon-presentation.svg",
-      alt: "cartoon icon of person presenting a bar chart on presentation slide"
+    source: "Deliver presentations, reporting and support junior staff",
+    img: Presentationicon,
+    alt: "cartoon icon of person presenting a bar chart on presentation slide",
   },
 ];
