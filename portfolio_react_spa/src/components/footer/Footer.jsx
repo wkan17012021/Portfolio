e7e4d2d26350d1f1a3b38e9f1dfc366cb57eb955
wkan17012021 <wkan@hotmail.co.uk>
2023-08-01 22:48:34 +0100

    minor formatting to footer socials

--- portfolio_react_spa/src/components/footer/Footer.jsx
+++ portfolio_react_spa/src/components/footer/Footer.jsx
@@ -55,7 +55,7 @@ function Footer() {
               justifyContent: "center",
               borderTop: "4px solid #407bff",
               borderBottom: "4px solid #407bff",
-              transition: "all 1s ease-in-out",
+              transition: "all 2s ease-in-out",
               "&:hover": {
                 border: "none",
               },
@@ -121,7 +121,7 @@ function Footer() {
             </Tooltip>
           </ListItem>
           <ListItem>
-            <Tooltip title="Share knowledge on HashNode" placement="top" arrow>
+            <Tooltip title="Share knowledge on HashNode" placement="bottom" arrow>
               <Link
                 hrefLang="en"
                 target="_blank"
